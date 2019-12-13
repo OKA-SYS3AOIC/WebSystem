ce24924735dc7b57b7b934dcad447120788296f2
OKA-SYS3AOIC <b7069@oic.jp>
2019-12-13 13:33:16 +0900

    js部分を分割

---
+++ 20191108/js/sanitizing.js
@@ -0,0 +1,9 @@
+﻿function checkForm2($this)
+{
+    var str=$this.value;
+    while(str.match(/[!"#$%&'()*,\-.\/:;<>?@\[\\\]\^_`{|}~]/))
+    {
+        str=str.replace(/[!"#$%&'()*,\-.\/:;<>?@\[\\\]\^_`{|}~]/,"");
+    }
+    $this.value=str;
+}
--- 20191108/room.php
+++ 20191108/room.php
@@ -231,63 +231,10 @@ for($fe=1; $fe<=$_SESSION['count']; $fe++){
 }
 ?>
 </form>
-<script type="text/javascript">
-var elems = document.getElementsByName("newsel");
-for(var i = 0; i <elems.length; i++){
-}
-function formCheck(){
-    var flag = 0;
-    var input_text_1_length = document.newrecform.newclassinput.value.length;
-    var input_text_2_length = document.newrecform.newclasstype.value.length;
-    if ( elems[0].checked==true&&input_text_1_length < 2 ){
-        flag = 1;
-        document . getElementById( 'notice-input-text-1' ) . innerHTML = "文字数不足です。";
-        document . getElementById( 'notice-input-text-1' ) . style . display = "block";
-    }
-    if ( elems[0].checked==true&&input_text_1_length  > 3 ){
-        flag = 1;
-        document . getElementById( 'notice-input-text-1' ) . innerHTML = "3文字以内でお願いします。";
-        document . getElementById( 'notice-input-text-1' ) . style . display = "block";
-    }
-    if ( elems[1].checked==true&&input_text_2_length < 1 ){
-        flag = 1;
-        document . getElementById( 'notice-input-text-1' ) . innerHTML = "教室タイプ名を入力してください。";
-        document . getElementById( 'notice-input-text-1' ) . style . display = "block";
-    }
-    if ( elems[1].checked==true&&input_text_2_length  > 30 ){
-        flag = 1;
-        document . getElementById( 'notice-input-text-1' ) . innerHTML = "30文字以内でお願いします。";
-        document . getElementById( 'notice-input-text-1' ) . style . display = "block";
-    }
-    if( flag ){
-        return false;
-    }else{
-        document . getElementById( 'notice-input-text-1' ) . style . display = "none";
-        return true;
-    }
-}
-//英数字のみ入力させる
-function checkForm($this)
-{
-    var str=$this.value;
-    while(str.match(/[^A-Z^a-z\d\-]/))
-    {
-        str=str.replace(/[^A-Z^a-z\d\-]/,"");
-    }
-    $this.value=str;
-}
+<script src="js/inputcheck.js"></script>
 
-//サニタイジング(SQLi対策)
-function checkForm2($this)
-{
-    var str=$this.value;
-    while(str.match(/[!"#$%&'()*,\-.\/:;<>?@\[\\\]\^_`{|}~]/))
-    {
-        str=str.replace(/[!"#$%&'()*,\-.\/:;<>?@\[\\\]\^_`{|}~]/,"");
-    }
-    $this.value=str;
-}
+<script src="js/EngNumOnly.js"></script>
 
-</script>
+<script src="js/sanitizing.js"></script>
 </body>
 </html>
